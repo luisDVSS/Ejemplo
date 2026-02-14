af1c792f11a4269c7a320737658794d7787892e0
luisd <8elilustrador8@gmail.com>
2026-02-14 23:34:43 +0000

    linux dns

--- linux/dns/main.sh
+++ linux/dns/main.sh
@@ -7,13 +7,22 @@ while :; do
     read -r opc
     case "$opc" in
     1)
-        if isInstalled bind bind9utils bind9-doc; then
-            echo "Los servicios de DNS se service estan intalado."
+        if isInstalled bind9 bind9utils bind9-doc; then
+            echo "Los servicios de DNS service YA estan intalados."
+        else
+            echo "Los servicios de DNS NO estan instalado."
+        fi
+
+        ;;
+    2)
+        echo "Validando la instalacion de bind9..."
+        if isInstalled bind9 bind9-doc bind9utils; then
+            echo "Ya esta intalado"
             read -r -p "¿Deses continuar con la configuracion?"[s/n] cont
             case $cont in
             s)
                 echo "Continuando con la configuracion..."
-
+                #llamar setConfig
                 ;;
             n)
                 echo "saliendo del script.."
@@ -21,22 +30,10 @@ while :; do
                 ;;
 
             esac
-
-        else
-            echo "Los servicios de DNS No estan instalado."
-            apt update &>/dev/null
-            getService bind9 bind9-doc bind9utils
-        fi
-
-        ;;
-    2)
-        echo "Validando la instalacion de bind9..."
-        if isInstalled bind9; then
-            echo "Ya esta intalado"
             continue
         else
             echo "procediendo con la instalacion de bind9.."
-            getService bind9
+            getService bind9 bind9-doc bind9utils
         fi
         ;;
     *)
